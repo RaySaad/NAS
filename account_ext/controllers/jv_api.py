691808262770ff67ea66c6c42f45e9914d74808f
Ayyan <ayyansaddiqui420@gmail.com>
2026-02-23 15:27:56 +0500

    adding the customer account on lines on payment api

--- account_ext/controllers/jv_api.py
+++ account_ext/controllers/jv_api.py
@@ -804,21 +804,39 @@ class JVAPI(http.Controller):
 										'data': {}
 									}})
 
-							vals = {
-								'account_id': request.env['account.account'].sudo().search([
-									('code', '=', line['account_id'])]).id,
-								'name': line['name'],
-								'employee_id': existing_employee.id if existing_employee else False,
-								'employee_code': line.get('employee_code', ''),
-								"customer_code": line.get('customer_code', ''),
-								"operating_unit_id": line_operating_unit.id,
-								"customer_account": partner_contract.id if partner_contract else False,
-								'contract_type': partner_contract.contract_type if partner_contract else '',
-								'analytic_distribution': analytic_distribution or {},
-								'tax_ids': [[4, line['tax_id']]] if line.get('tax_id', False) else [],
-								'debit': line['amount'] if line['type'] == 'debit' else 0,
-								'credit': line['amount'] if line['type'] == 'credit' else 0
-							}
+							if partner_contract:
+								vals = {
+									'account_id': request.env['account.account'].sudo().search([
+										('code', '=', line['account_id'])]).id,
+									'name': line['name'],
+									'employee_id': existing_employee.id if existing_employee else False,
+									'employee_code': line.get('employee_code', ''),
+									"customer_code": line.get('customer_code', ''),
+									"operating_unit_id": line_operating_unit.id,
+									"customer_account": partner_contract.id if partner_contract else False,
+									'contract_type': partner_contract.contract_type if partner_contract else '',
+									'partner_id': partner_contract.partner_id if partner_contract else '',
+									'analytic_distribution': analytic_distribution or {},
+									'tax_ids': [[4, line['tax_id']]] if line.get('tax_id', False) else [],
+									'debit': line['amount'] if line['type'] == 'debit' else 0,
+									'credit': line['amount'] if line['type'] == 'credit' else 0
+								}
+							else:
+								vals = {
+									'account_id': request.env['account.account'].sudo().search([
+										('code', '=', line['account_id'])]).id,
+									'name': line['name'],
+									'employee_id': existing_employee.id if existing_employee else False,
+									'employee_code': line.get('employee_code', ''),
+									"customer_code": line.get('customer_code', ''),
+									"operating_unit_id": line_operating_unit.id,
+									"customer_account": partner_contract.id if partner_contract else False,
+									'contract_type': partner_contract.contract_type if partner_contract else '',
+									'analytic_distribution': analytic_distribution or {},
+									'tax_ids': [[4, line['tax_id']]] if line.get('tax_id', False) else [],
+									'debit': line['amount'] if line['type'] == 'debit' else 0,
+									'credit': line['amount'] if line['type'] == 'credit' else 0
+								}
 
 							line_val.append((0, 0, vals))
 
