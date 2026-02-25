bf90645fc2d261b9e4870371522776bd87444dd6
hamzairfan <hamzairfangill@outlook.com>
2026-02-25 20:21:26 +0500

    pc to ou mapping added and cron created to update based on the mapping

--- employees_ext/__manifest__.py
+++ employees_ext/__manifest__.py
@@ -3,14 +3,17 @@
     'version': '1.0',
     'category': 'Human Resources',
 
-    'depends': ['hr', 'base','mail','base_setup', 'operating_unit'],
+    'depends': ['hr', 'base', 'mail', 'base_setup', 'operating_unit'],
     'data': [
         'security/ir.model.access.csv',
+        'data/pc_ou_mapping_data.xml',
+        'data/naas_ou_cron.xml',
         'views/employee_views.xml',
         'views/employee_section_views.xml',
         'views/res_config.xml',
+        'views/pc_ou_mapping_views.xml',
     ],
-    
+
     'license': 'LGPL-3',
     'application': True,
     'installable': True,
