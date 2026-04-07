b84ccf6746d9478e95091df5be43c288d7c12553
hamzairfan <hamzairfangill@outlook.com>
2026-04-07 17:36:08 +0500

    [Fix] added empty domain for journal_id field on JV

--- account_ext/models/account_move.py
+++ account_ext/models/account_move.py
@@ -31,6 +31,7 @@ class AccountMove(models.Model):
 		compute='_compute_journal_id', inverse='_inverse_journal_id', store=True, readonly=False, precompute=True,
 		required=True,
 		check_company=True,
+        domain=[]
 	)
 
 	jv_type = fields.Selection(
