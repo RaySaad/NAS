8d29952903f9e4c1c8e36a7dc2e1ecd76aaceee9
hamzairfan <hamzairfangill@outlook.com>
2026-04-07 17:19:16 +0500

    [UPDATE] Removed domain from journal ID on JV

--- account_ext/models/account_move.py
+++ account_ext/models/account_move.py
@@ -31,7 +31,6 @@ class AccountMove(models.Model):
 		compute='_compute_journal_id', inverse='_inverse_journal_id', store=True, readonly=False, precompute=True,
 		required=True,
 		check_company=True,
-		domain="[('id', 'not in', suitable_journal_ids)]",
 	)
 
 	jv_type = fields.Selection(
